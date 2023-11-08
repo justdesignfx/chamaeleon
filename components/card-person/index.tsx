ce10600f192d3519c79e84d996a2cbbcba44cde0
armagansenol <armagansnl@gmail.com>
2023-11-08 10:27:27 +0300

    update team

--- components/card-person/index.tsx
+++ components/card-person/index.tsx
@@ -1,9 +1,34 @@
+import { ICardPerson } from "@/global"
 import React from "react"
+import s from "./card-person.module.scss"
+import CustomImage from "../custom-image"
+import { CustomLink } from "../custom-link"
 
-type Props = {}
+import cn from "clsx"
+
+type Props = ICardPerson
 
 const CardPerson = (props: Props) => {
-  return <div>CardPerson</div>
+  return (
+    <div className={cn(s.cardPerson, "cursor-pointer")}>
+      <div className={s.imgC}>
+        <div>
+          <CustomImage src={props.img} alt="Profile Photo of a Team Member" height="500" width="500" />
+        </div>
+      </div>
+      <p>{props.name}</p>
+      <small>{props.role}</small>
+      <CustomLink href={props.social.url} className={cn(s.iconC, "flex-center")}>
+        <CustomImage
+          src={props.social.icon}
+          alt="Profile Photo of a Team Member"
+          height="500"
+          width="500"
+          style={{ objectFit: "contain" }}
+        />
+      </CustomLink>
+    </div>
+  )
 }
 
 export default CardPerson
--- pages/the-team/index.tsx
+++ pages/the-team/index.tsx
@@ -2,16 +2,56 @@ import React from "react"
 import s from "./team.module.scss"
 import DefaultLayout from "@/layouts/default"
 import Header from "@/components/header"
+import { cardPerson } from "@/global"
+import CardPerson from "@/components/card-person"
+import CustomImage from "@/components/custom-image"
+import Button from "@/components/button"
 
-type Props = {}
+const TheTeam = () => {
+  const members = [cardPerson, cardPerson, cardPerson, cardPerson, cardPerson, cardPerson]
 
-const TheTeam = (props: Props) => {
   return (
     <DefaultLayout>
       <Header />
-
       <section className={s.intro}>
         <h1>THE TEAM</h1>
+      </section>
+      <section className={s.members}>
+        {members.map((item, i) => {
+          return <CardPerson key={i} {...item} />
+        })}
+      </section>
+      <section className={s.achievements}>
+        <div className={s.boxes}>
+          <div>
+            <h5>60+</h5>
+            <p>Investments led and executed to date.</p>
+          </div>
+          <div>
+            <h5>40+</h5>
+            <p>Countries, a vast network of deep relationships.</p>
+          </div>
+          <div>
+            <h5>500+</h5>
+            <p>
+              people managed, <span>$100m+</span> yearly budgets.
+            </p>
+          </div>
+          <div>
+            <h5>TOP 2 TO 5</h5>
+            <p>Percentile returns as VC investors.</p>
+          </div>
+        </div>
+
+        <div className={s.imgC}>
+          <div>
+            <CustomImage src="/img/bale.png" alt="Dollar Bale" height="500" width="500" />
+          </div>
+        </div>
+      </section>
+      <section className={s.link}>
+        <small>Discover our investment style</small>
+        <Button text="APPROACH" path="/approach" size="lg" />
       </section>
     </DefaultLayout>
   )

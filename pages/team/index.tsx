ec367fffcab618d03760c058cb391c4db052b5e4
armagansenol <armagansnl@gmail.com>
2023-11-13 15:01:01 +0300

    add detail modal

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -1,40 +1,72 @@
+import { useEffect, useState } from "react"
 import s from "./team.module.scss"
 
-import Button from "@/components/button"
-import CardPerson from "@/components/card-person"
-import CustomImage from "@/components/custom-image"
+import cn from "clsx"
+
 import Parallax from "@/components/animations/parallax"
 import Reveal from "@/components/animations/reveal"
-import { cardPerson } from "@/global"
+import Button from "@/components/button"
+import CardInfo from "@/components/card-info"
+import CardPerson from "@/components/card-person"
+import CustomImage from "@/components/custom-image"
+import DetailSlider from "@/components/detail-slider"
+import { cardPerson, cardPerson2, cardPerson3 } from "@/global"
 import DefaultLayout from "@/layouts/default"
+import { useModalStore } from "@/lib/store/modal"
 
 const Team = () => {
+  const modalStore = useModalStore()
+  const [selected, setSelected] = useState<number | null>(null)
   const members = [
     cardPerson,
+    cardPerson2,
+    cardPerson3,
     cardPerson,
     cardPerson,
     cardPerson,
     cardPerson,
+    cardPerson2,
+    cardPerson3,
     cardPerson,
     cardPerson,
     cardPerson,
+    cardPerson2,
+    cardPerson3,
     cardPerson,
     cardPerson,
     cardPerson,
+    cardPerson2,
+    cardPerson3,
     cardPerson,
     cardPerson,
     cardPerson,
-    cardPerson,
-    cardPerson,
-    cardPerson,
-    cardPerson,
-    cardPerson,
-    cardPerson,
-    cardPerson,
-    cardPerson,
+    cardPerson2,
+    cardPerson3,
     cardPerson,
     cardPerson,
   ]
+
+  const handleModal = (index: number) => {
+    setSelected(index)
+  }
+
+  useEffect(() => {
+    if (selected === null) return
+
+    modalStore.setContent(
+      <DetailSlider
+        currentSlide={selected}
+        slides={members.map((member, i) => {
+          return (
+            <div className={cn(s.slide, "flex-center")} key={i}>
+              <CardInfo {...member} />
+            </div>
+          )
+        })}
+      />
+    )
+    modalStore.setIsOpen(true)
+  }, [selected])
 
   return (
     <DefaultLayout>
@@ -46,9 +78,11 @@ const Team = () => {
       <section className={s.members}>
         {members.map((item, i) => {
           return (
-            <Reveal key={i}>
-              <CardPerson {...item} />
-            </Reveal>
+            <div key={i} onClick={() => handleModal(i)}>
+              <Reveal>
+                <CardPerson {...item} />
+              </Reveal>
+            </div>
           )
         })}
       </section>

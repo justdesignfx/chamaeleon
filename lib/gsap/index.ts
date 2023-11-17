dbc8ed95fc755b693328572c78e3de6933907256
armagansenol <armagansnl@gmail.com>
2023-11-17 13:46:49 +0300

    remove comments

--- lib/gsap/index.ts
+++ lib/gsap/index.ts
@@ -8,16 +8,15 @@ const RECIPROCAL_GR = 1 / GOLDEN_RATIO
 const DURATION = RECIPROCAL_GR
 const EASE = CustomEase.create("ease", "0.175, 0.885, 0.32, 1")
 
-// Configuring GSAP with custom settings that aren't Tween-specific
 gsap.config({
   autoSleep: 60,
 })
 
-// Setting default animation properties that should be inherited by ALL tweens
 gsap.defaults({
   duration: DURATION,
   ease: EASE,
 })
 
-// Once the desired configurations are set, we simply export what we need to work with in the future.
+ScrollTrigger.defaults({ markers: process.env.NEXT_PUBLIC_NODE_ENV === "development" })
+
 export { ScrollTrigger, CustomEase, DURATION, EASE, GOLDEN_RATIO, gsap }

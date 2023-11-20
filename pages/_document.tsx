f3d0c8e8ec8b1afcc3dff675d874cd935dfb3624
armagansenol <armagansnl@gmail.com>
2023-11-20 14:53:38 +0300

    preload fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,32 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <link
+          href="/fonts/FirstAidKitF-Bold.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/HelveticaNeue-Bold.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="/fonts/HelveticaNeue-Roman.woff2"
+          as="font"
+          rel="preload prefetch"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />

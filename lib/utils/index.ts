829c2a6d07173107aa857a278cddd1170f189aa9
armagansenol <armagansnl@gmail.com>
2023-11-21 16:12:09 +0300

    add capitalize

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -6,9 +6,16 @@ export function lineBreak(text: string) {
   return text.replace("<br>", "\n")
 }
 
-export const truncateString = (str: string, num: number) => {
+export function truncateString(str: string, num: number) {
   if (str.length <= num) {
     return str
   }
   return str.slice(0, num) + "..."
+}
+
+export function capitalize(sentence: string): string {
+  const words: string[] = sentence.split(" ")
+  const capitalizedWords: string[] = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+  const result: string = capitalizedWords.join(" ")
+  return result
 }

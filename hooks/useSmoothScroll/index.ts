f4d7c178449724d352b7d1b0d21a91b12d7d4bb3
armagansenol <armagansnl@gmail.com>
2023-11-28 13:28:22 +0300

    remove easing

--- hooks/useSmoothScroll/index.ts
+++ hooks/useSmoothScroll/index.ts
@@ -25,7 +25,7 @@ const useSmoothScroll = () => {
   useIsomorphicLayoutEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+      // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
       orientation: "vertical",
       gestureOrientation: "vertical",
       smoothWheel: true,

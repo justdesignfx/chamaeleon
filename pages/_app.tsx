053477f2653ce76ddd89bb18bf97ea8689198ae2
armagansenol <armagansnl@gmail.com>
2023-12-05 09:56:46 +0300

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app"
 import "../styles/global.scss"
 
-import { CustomEase, ScrollTrigger, gsap } from "@/lib/gsap"
 import { QueryClient, QueryClientProvider } from "react-query"
 import { useIsomorphicLayoutEffect } from "usehooks-ts"
 
@@ -13,22 +12,12 @@ import { useModalStore } from "@/lib/store/modal"
 
 const queryClient = new QueryClient()
 
-if (typeof window !== "undefined") {
-  // reset scroll position
-  window.scrollTo(0, 0)
-  window.history.scrollRestoration = "manual"
-}
-
 export default function App({ Component, pageProps }: AppProps) {
   const modalStore = useModalStore()
   // const router = useRouter()
   useSmoothScroll()
 
   // const [loading, setLoading] = useState(false)
-
-  useIsomorphicLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger, CustomEase)
-  }, [])
 
   useIsomorphicLayoutEffect(() => {
     if (localStorage.getItem("cookieAccepted")) return

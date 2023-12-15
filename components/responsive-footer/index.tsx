3bc0bc4279a3f61987840940dde5dc6ae463a4a6
armagansenol <armagansnl@gmail.com>
2023-12-15 15:04:41 +0300

    fix reveal

--- components/responsive-footer/index.tsx
+++ components/responsive-footer/index.tsx
@@ -1,13 +1,16 @@
 import { breakpoints } from "@/lib/utils"
 import { useMediaQuery } from "usehooks-ts"
-import { FooterReveal } from "../animations/footer-reveal"
-import { Footer } from "../footer"
+
+import { FooterReveal } from "@/components/animations/footer-reveal"
+import { Footer } from "@/components/footer"
+
+import { ClientOnly } from "@/hocs/isomorphic"
 
 const ResponsiveFooter = () => {
   const isMobile = useMediaQuery(`(max-width: ${breakpoints.mobile}px)`)
 
   return (
-    <>
+    <ClientOnly>
       {isMobile ? (
         <Footer />
       ) : (
@@ -15,7 +18,7 @@ const ResponsiveFooter = () => {
           <Footer />
         </FooterReveal>
       )}
-    </>
+    </ClientOnly>
   )
 }
 

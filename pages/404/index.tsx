2a156e93c2404bc605bd4e29e393bb5dd36dfe44
armagansenol <armagansnl@gmail.com>
2023-12-18 11:44:49 +0300

    fix

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -1,20 +1,20 @@
-import { NonFooter } from "@/layouts/non-footer"
 import s from "./not-found.module.scss"
 
 import cn from "clsx"
 
 import { Button } from "@/components/button"
+import { NonFooterLayout } from "@/layouts/non-footer"
 
 import { routes } from "@/constants"
 
 const NotFound = () => {
   return (
-    <NonFooter seo={{ title: "Page Not Found", description: "We couldn't find the page you were looking for." }}>
+    <NonFooterLayout seo={{ title: "Page Not Found", description: "We couldn't find the page you were looking for." }}>
       <div className={cn(s.notFound, "flex-center-y")}>
         <h1>PAGE NOT FOUND</h1>
         <Button path={`/${routes.home.path}`} text="MAIN PAGE" size="md" />
       </div>
-    </NonFooter>
+    </NonFooterLayout>
   )
 }
 

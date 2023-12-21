3bb8b9d439027c915ed2561bc2554010761901c4
armagansenol <armagansnl@gmail.com>
2023-12-21 15:28:13 +0300

    test footer

--- components/responsive-footer/index.tsx
+++ components/responsive-footer/index.tsx
@@ -1,21 +1,10 @@
-import { breakpoints } from "@/lib/utils"
-import { useMediaQuery } from "usehooks-ts"
-
 import { FooterReveal } from "@/components/animations/footer-reveal"
 import { Footer } from "@/components/footer"
 
 const ResponsiveFooter = () => {
-  const isMobile = useMediaQuery(`(max-width: ${breakpoints.mobile}px)`)
-
   return (
     <div>
-      {isMobile ? (
-        <Footer />
-      ) : (
-        <FooterReveal>
-          <Footer />
-        </FooterReveal>
-      )}
+      <Footer />
     </div>
   )
 }

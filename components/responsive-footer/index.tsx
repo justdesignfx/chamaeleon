0704ec89fe1b16e4709f7b956d54fe2aea144032
armagansenol <armagansnl@gmail.com>
2023-12-21 12:32:29 +0300

    test

--- components/responsive-footer/index.tsx
+++ components/responsive-footer/index.tsx
@@ -1,18 +1,17 @@
 import { breakpoints } from "@/lib/utils"
-import { useIsomorphicLayoutEffect, useMediaQuery } from "usehooks-ts"
+import { useMediaQuery } from "usehooks-ts"
 
 import { FooterReveal } from "@/components/animations/footer-reveal"
 import { Footer } from "@/components/footer"
 
 import { ClientOnly } from "@/hocs/isomorphic"
-import { ScrollTrigger } from "@/lib/gsap"
 
 const ResponsiveFooter = () => {
   const isMobile = useMediaQuery(`(max-width: ${breakpoints.mobile}px)`)
 
-  useIsomorphicLayoutEffect(() => {
-    ScrollTrigger.refresh()
-  })
+  // useIsomorphicLayoutEffect(() => {
+  //   ScrollTrigger.refresh()
+  // })
 
   return (
     <ClientOnly>

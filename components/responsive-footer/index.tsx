9cfe9992953ad53ba421a5d12dfd342fcd7b426c
armagansenol <armagansnl@gmail.com>
2023-12-21 11:50:05 +0300

    add st refresh

--- components/responsive-footer/index.tsx
+++ components/responsive-footer/index.tsx
@@ -1,13 +1,18 @@
 import { breakpoints } from "@/lib/utils"
-import { useMediaQuery } from "usehooks-ts"
+import { useIsomorphicLayoutEffect, useMediaQuery } from "usehooks-ts"
 
 import { FooterReveal } from "@/components/animations/footer-reveal"
 import { Footer } from "@/components/footer"
 
 import { ClientOnly } from "@/hocs/isomorphic"
+import { ScrollTrigger } from "@/lib/gsap"
 
 const ResponsiveFooter = () => {
   const isMobile = useMediaQuery(`(max-width: ${breakpoints.mobile}px)`)
+
+  useIsomorphicLayoutEffect(() => {
+    ScrollTrigger.refresh()
+  })
 
   return (
     <ClientOnly>
